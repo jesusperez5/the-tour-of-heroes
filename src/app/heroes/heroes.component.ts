49a09d9d798e25464679424d75905700dc58329d
jesusperez5 <jesuspepr554@gmail.com>
2024-05-28 12:24:22 +0200

    Implementación de servicios de mensajes y heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -18,11 +20,24 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit{
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes); 
+    // Esto se hace así porque al añadir servicios el servidor funciona de forma asincrona
+  }
+  // Esto se ejecuta después del contructor, y se hace para dejar el constructor mas limpio
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
